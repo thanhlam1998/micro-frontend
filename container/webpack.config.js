8be70a15230e2939d6786541cda852e64f5c6fad
thanhlam1998 <lamnguyen11e@gmail.com>
2021-09-27 21:33:45 +0700

    import cart service to container

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -8,9 +8,10 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "products",
+      name: "container",
       remotes: {
         products: "products@http://localhost:8081/remoteEntry.js",
+        cart: "cart@http://localhost:8082/remoteEntry.js",
       },
     }),
     new HtmlWebpackPlugin({
